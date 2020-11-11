27ce02c94c7504d7348c08fa5939e357c264b9bb
Benjamin Cheng <bencheng200@gmail.com>
2020-11-11 21:21:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line (array)
+  str = 'The line is currently: '
+  count = 1
+  if array.length == 0
+    puts "The line is currently emtpy."
+  else 
+    array.each { |x|
+      str.join("#{count}. #{x}")
+      count +=1
+    }
+    return str
+  end
+end
