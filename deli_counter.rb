efffabc659fa12f527b1cd85388f7afd7d6276c3
Benjamin Cheng <bencheng200@gmail.com>
2020-11-11 21:30:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 def line (array)
-  str = "The line is currently: "
+  str = "The line is currently:"
   count = 1
   if array.length == 0
     puts "The line is currently empty."
   else
     array.each { |x|
-      str += "#{count}. #{x} "
+      str += " #{count}. #{x}"
       count +=1
     }
     puts str
