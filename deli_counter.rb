e47c542da64bcade88a51fcaf337a0021a35f095
Benjamin Cheng <bencheng200@gmail.com>
2020-11-11 21:29:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line (array)
-  str = "The line is currently: "
+  str = "The line is currently: s"
   count = 1
   if array.length == 0
     puts "The line is currently empty."
